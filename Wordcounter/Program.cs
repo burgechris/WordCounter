f9bd38c79a31b9efa6e8d1fff96c478429c5024c
Chris Burge <topher.burge@gmail.com>
2019-10-04 15:30:30 -0700

    add a few break lines to the console and fixed typos.

--- Wordcounter/Program.cs
+++ Wordcounter/Program.cs
@@ -8,16 +8,20 @@ namespace Wordcounter
         static void Main()
         {
             Console.BackgroundColor = ConsoleColor.DarkCyan;
-            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.ForegroundColor = ConsoleColor.DarkRed;
             Console.WriteLine("Hey There!");
+            Console.WriteLine("-----------------------------------------------------------");
             Console.WriteLine("Check to see how many times a word is repeated in a phrase.");
+            Console.WriteLine("-----------------------------------------------------------");
             Console.WriteLine("Please enter a phrase.");
             string phrase = Console.ReadLine().ToLower();
             Console.WriteLine("Please enter a word.");
             string word = Console.ReadLine().ToLower();
             WordCounter newWordCounter = new WordCounter(word, phrase);
             int count = newWordCounter.RepeatChecker();
-            Console.WriteLine("Your word, " + word + ", appears " + count + " times.");
+            Console.WriteLine("-----------------------------------------------------------");
+            Console.WriteLine("The word '" + word + "' appears " + count + " times.");
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
             Console.ResetColor();
         }
     }
